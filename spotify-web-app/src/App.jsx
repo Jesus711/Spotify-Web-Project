6fb93d3273658da037262995244700e1f9c96deb
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-06-10 18:38:16 -0700

    Modified the Top section to contain a logo and have a rotating animation and modified Playlist.jsx form creation

--- spotify-web-app/src/App.jsx
+++ spotify-web-app/src/App.jsx
@@ -4,14 +4,22 @@ import Login from './components/Login';
 import Home from './components/Home';
 import Playlist from './components/Playlist';
 import Collab from './components/Collab';
-
+import spotify_img from '../src/assets/spotify-look-logo.png'
 
 function App() {
 
 
   return (
     <div className="App">
-      <div className="App-title"><a href='.'>SpotifyCollab</a></div>
+      <div className="App-title">
+        <div className='container'>
+            <div className='logo-border'></div>
+            <img  className='logo-img'  src={spotify_img} alt="" />
+            {/* <div className='logo'></div> */}
+        </div>
+
+        <a href='.'>SpotifyCollab</a>
+      </div>
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
