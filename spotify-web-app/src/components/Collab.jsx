14f4f50e20257fe395f80b0bf4d2602e7fc55bb4
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2023-06-23 16:07:52 -0700

    Added Collab Playlist Page to allow user to enter shared token and display the playlist if found and allows user to begin adding/removing songs after clicking Edit playlist. If user clicks edit playlist, the user will follow the playlist

--- spotify-web-app/src/components/Collab.jsx
+++ spotify-web-app/src/components/Collab.jsx
@@ -1,11 +1,138 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import '../css/Collab.css';
-
-
+import { useState } from 'react';
+import PlaylistItem from './PlaylistItem';
 
 function Collab() {
 
+    const [collabToken, setCollabToken] = useState("");
+    const [inputToken, setInputToken] = useState("");
+    const [playlist, setPlaylist] = useState({});
+    const [ownerToken, setOwnerToken] = useState("");
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    console.log(location.state)
+
+    async function getCollabPlaylist(e) {
+
+        e.preventDefault()
+
+        let collabPlaylist = collabToken.split("/")
+        if(collabPlaylist.length <= 1){
+            return null;
+        }
+        console.log(collabPlaylist)
+
+        let owner_token = collabPlaylist[1];
+        setOwnerToken(owner_token)
+
+        let result = await fetch(`https://api.spotify.com/v1/playlists/${collabPlaylist[0]}`,//?${song}&position=0`, 
+        {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${location.state.token}`,
+                'Content-Type': 'application/json',
+            },
+        }).then(res => {
+            console.log(res)
+            if (res.status >= 400){
+                setPlaylist({})
+                setInputToken(null)
+                return null;
+            }
+            return res.json()
+        })
+
+        if(result === null){
+            return null
+        }
+
+        setInputToken(collabToken)
+        console.log(result)
+        let id = `"${result.id}"`;
+        let name = result.name;
+        let images = result.images;
+        let desc = result.description;
+        let total = result.tracks.total;
+        let publicPlay = result.public;
+        let collab = result.collaborative;
+        let owner = result.owner.display_name;
+        let playlist = {"id" : id, "owner": owner, "name" : name, "image": images, "description" : desc, "total" : total, "public" : publicPlay, "collaborative": collab, "shared": 1, "info" : []}
+        for(let track of result.tracks.items){
+                let tid = track.track.id
+                let tname = track.track.name
+                let artists = track.track.artists
+                let preview = track.track.preview_url
+                let trackObj = {}
+                trackObj["id"] = tid;
+                trackObj["name"] = tname;
+                trackObj["artists"] = artists;
+                trackObj["preview"] = preview;
+                trackObj["images"] = track.track.album.images;
+                playlist["info"].push(trackObj);
+            }
+        console.log(playlist)
+        setPlaylist(playlist)
+    }
+
+    const handleEditPlaylist = async (id) => {
+
+        console.log(id)
+        let playlist_id = `${id}`.replace('\"', "").replace('\"', "")
+        console.log(playlist_id)
+
+        // Follow Playlist
+        let result = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/followers`,{
+            method: "PUT",
+            headers: {
+                'Authorization': `Bearer ${location.state.token}`,
+                'Content-Type': 'application/json',
+            },
+        }).then(res => {
+            console.log(res)
+        })
+
+
+
+        navigate('/playlist/search', {replace: false, state: {token: location.state.token, id: playlist_id, country: location.state.country, "shared": 1, "owner_token": ownerToken} })
+    }
+
+
+    const handleCollabTokenSearch = () => {
+
+
+        if(playlist.id === undefined && inputToken === null){
+            return (
+                <div>No Playlist Found!</div>
+            )
+        }
+        else if (playlist.id === undefined){
+            return
+        }
+        
+        return (
+        <div className='collab-result'>
+            <div className='result-text'>Playlist Found</div>
+            <div className='result-text'>Owner: {playlist.owner}</div>
+            <PlaylistItem playlist={playlist} handleEditPlaylist={handleEditPlaylist}/>
+        </div>
+        )
+    }
+
+
+
+
     return (
-        <div>COLLABING: ENTER SHARE TOKEN!</div>
+        <div className='collab-playlist'>
+            <form className="collab-playlist-form" onSubmit={(e) => {getCollabPlaylist(e)}}>
+                <div><strong>Collab Playlist</strong></div>
+                <input id="name-input" value={collabToken} onChange={(e) => setCollabToken(e.target.value)} type="text" placeholder="Enter Share Token"/>
+                <button id="create-btn"  type="submit">Collab</button>
+            </form>
+            {handleCollabTokenSearch()}
+        </div>
     )
 }
 
